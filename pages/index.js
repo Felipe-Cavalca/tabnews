994e5eeb13fad38decf1c6b25b8ba32d1ad8c6b1
Felipe Cavalca <flpsnocvla@gmail.com>
2025-11-23 00:48:39 +0000

    Criando mensagem para amiga

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>hello world</h1>
+    return <h1>Darlinha, obrigado por todos esses anos de amizade e por tudo que já vivemos juntos. Sua amizade é um presente pra mim. Estarei sempre aqui quando você precisar, de verdade.</h1>
 }
 
 export default Home;
